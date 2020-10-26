54f027a227d4e8fa5b646bd529adb4ee03fca6a1
whitewiz13 <sandeepthakur130197@gmail.com>
2020-10-26 14:42:35 +0530

    Init

---
+++ server.js
@@ -0,0 +1,28 @@
+//Required Packages
+const express = require('express');
+const cors = require('cors'); 
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Setting port for live project
+const port = process.env.PORT || 3001;
+
+//-------Build Code (Server React pages)------
+const publicPath  = path.join(__dirname, 'client/build');
+app.use(express.static(publicPath));
+
+//-------Get methods---------
+app.get('/', function (req, res) {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port,()=>{
+    console.log("Server running at port : ",port);
+});
